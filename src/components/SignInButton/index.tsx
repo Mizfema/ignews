8b8bb4931aa9965161943ebde90093135ba652f8
Mizael <mizaellmagumba@gmail.com>
2022-08-20 13:27:21 +0200

    Falha no redirecionamento para stripe

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -6,9 +6,9 @@ import {signIn,signOut, useSession} from 'next-auth/react'
 
 
 export function SignInButton() {
-
   const {data: session} = useSession();
   console.log(session)
+
 
   return session ? 
   (
